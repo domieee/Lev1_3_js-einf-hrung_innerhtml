b5e5cf21145d12b65b5a7659084a441466bd36f3
domieee <99103235+domieee@users.noreply.github.com>
2023-01-04 18:09:52 +0100

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,33 @@
 Nutze den HTML-Code aus dem Kommentarbereich.
 */
 
-for (let i = 0; i < 3; i++) {
+// Mit der .innerHTML-Methode:
+let gallery = document.getElementById('gallery');
+gallery.innerHTML += '<figure><img src="https://picsum.photos/200/300" alt=""><figcaption>Fig.1</figure>';
+gallery.innerHTML += '<figure><img src="https://picsum.photos/200/300" alt=""><figcaption>Fig.2</figure>';
+gallery.innerHTML += '<figure><img src="https://picsum.photos/200/300" alt=""><figcaption>Fig.3</figure>';
+gallery.style.display = "flex";
+gallery.style.justifyContent = "center";
+
+/* for (let i = 0; i < 3; i++) { */
 
     // Die <figcaption>-Inhalte werden in einem Arry gespeichert.
     // Ebenso alle anderen DOM-Elemente die erzeugt werden müssen werden als Variable gespeichert
 
-    let captions = ["fig. 1", "fig. 2", "fig. 3"]
+/*  let captions = ["fig. 1", "fig. 2", "fig. 3"]
     let gallery = document.getElementById("gallery");
     let figure = document.createElement("figure");
     let img = document.createElement("img");
     let figcaption = document.createElement("figcaption");
-    img.src = "https://images.unsplash.com/photo-1672699118104-e1e3d815d486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
+    img.src = "https://images.unsplash.com/photo-1672699118104-e1e3d815d486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"; */
 
     // Durch den Loop wird 3x das Nesting von 'figure > img figcaption' erzeugt
 
-    gallery.appendChild(figure);
+/*  gallery.appendChild(figure);
     figure.appendChild(img);
-    figure.appendChild(figcaption);
+    figure.appendChild(figcaption); */
 
     // Die nummerische Position in der Iteration wird genutzt um die entsprechenden Inhalte für die <figcapton>-Elemente festzulegen.
 
-        figcaption.textContent = captions[i];  
-}
+/*         figcaption.textContent = captions[i];  
+} */
